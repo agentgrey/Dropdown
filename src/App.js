3e7d4584793fa7cee832d3bc5ad82382c4e8f605
agentgrey <himadrinayak2702@gmail.com>
2023-06-15 14:14:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+/** ------------------ IMPORTING CSS ------------------ **/
+import "./index.css";
+/** ------------------ IMPORTING COMPONENTS ------------------ **/
+import Dropdown from "./Dropdown/dropdown";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Dropdown />
     </div>
   );
 }
 
+/** ------------------ EXPORTING MODULES ------------------ **/
 export default App;
